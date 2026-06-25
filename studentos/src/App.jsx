2a68bbb5bee987e7ce44246816cb4579def55dd0
KESHAV <kesh8av5@gmail.com>
2026-06-25 23:00:57 +0530

    added router

--- studentos/src/App.jsx
+++ studentos/src/App.jsx
@@ -2,10 +2,26 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-import './App.css'
+import {Routes,Route} from 'react-router-dom'
+import './styles/App.css'
+import {Home} from './home.jsx'
+import {Progress} from './Progress.jsx'
+import {Task} from './Task.jsx'
+import {Calculator} from './Calculator.jsx'
+import {Courses} from './Courses.jsx'
 
 function App() {
-  
+    return (
+  <Routes>
+
+    <Route path='/' element={<Home />} />
+    <Route path='/Progress' element={<Progress />} />
+    <Route path='/Task' element={<Task />} />
+    <Route path='/Calculator' element={<Calculator />} />
+    <Route path='/Courses' element={<Courses />} />
+    
+  </Routes>
+  )
 }
 
 export default App
