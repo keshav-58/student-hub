d10bd815985f4110e8a89ebd498601ab608a5e5b
KESHAV <kesh8av5@gmail.com>
2026-07-01 01:19:37 +0530

    added control input and del task buttons code

--- studentos/src/Task.jsx
+++ studentos/src/Task.jsx
@@ -1,41 +1,71 @@
 import { useState } from "react";
 
-function Header(){
+function Header({setTask,text,setText}){
+    
+
+    function inputToTask(event){
+       setText(event.target.value);
+    }
+    function addTask(){
+        setTask((prev)=> [...prev,{id:crypto.randomUUID(),text:text,compeleted:false}]);
+
+        setText("");
+    }
+    
     return (
         <>
-        <input />
-        <button>+Task</button>
+        <input value={text} onChange={inputToTask} />
+        <button onClick={addTask}>+Task</button>
         </>
     )
 }
-function ShowTask({tasks}){
+function ShowTask({tasks,setCompeleted,text}){
 
     return (
         <>
-        
         {
          tasks.map((task,index)=>{
 
+            function delTask(id){
+                setCompeleted(prev=>
+                    prev.filter(item=> 
+                        item.id!==id
+                    )
+                )
+            } 
+            function editTask(id){
+                setCompeleted(prev=>
+                    prev.map(item=>
+                        item.id===id?{...item,text:text}:item
+                    )
+                )
+            }
             return (
-                <div key={index}>
-                <button></button> 
-                <p>{task}</p>
-                <button>EDIT</button>
-                <button>DEL</button>
+                <div key={task.id}>
+                <button onClick={()=>{
+                    setCompeleted( (prev) =>{
+                        return prev.map((item)=>{
+                            return item.id === task.id?{...item,compeleted:!item.compeleted}:item
+                     } )
+                    })
+                }}></button> 
+                <p>{task.text}</p>
+                <button onClick={()=> editTask(task.id)}>EDIT</button>
+                <button onClick={()=> delTask(task.id)} >DEL</button>
                 </div>
             )
          })
-        }
-        
+        } 
         </>
     )
 }
 function Todo(){
-    const [tasks,setTasks]=useState(["task 1" , "task 2", "task 3"]);
+    const [tasks,setTasks]=useState([]);
+    const[text,setText]=useState("");
     return (
         <>
-        <Header />
-        <ShowTask tasks={tasks} />
+        <Header setTask={setTasks} text={text} setText={setText} />
+        <ShowTask tasks={tasks} setCompeleted={setTasks} text={text} />
         </>
     )
 }
