662e76ce68e8c85f85b18aecf11937ed15d3b297
KESHAV <kesh8av5@gmail.com>
2026-07-07 19:26:02 +0530

    added calculator page and todo/cal css

--- studentos/src/Calculator.jsx
+++ studentos/src/Calculator.jsx
@@ -1,3 +1,50 @@
+import {useState} from 'react'
+function Header({answer,setAnswer}){
+    
+    return(
+    <div className="flex items-center justify-center mb-4">
+     <input readOnly placeholder='2+2=4' value={answer} type='textarea'
+     className='h-[120px] w-[408px] rounded text-right pr-2 text-4xl border border-gray-400 bg-gray-500 outline-none text-white' />
+     </div>
+    )
+}
+
+function Buttons({buttonValues,answer,setAnswer}){
+
+    function logic(val){
+
+        if(val==="C"){
+            setAnswer("")
+            return;
+        }
+        if(val==="="){
+            setAnswer(eval(answer).toString())
+        }else{
+            setAnswer(prev=>prev+val)
+        }
+    }
+    return <div className='grid grid-cols-4 gap-2'>
+        {buttonValues.map(nums=><button key={nums} value={nums}
+            onClick={()=>logic(nums)} 
+            className={`h-24 w-24 p-1 rounded-[10px] bg-blue-300 hover:bg-blue-500 
+                ${nums==="="?"bg-orange-500 hover:bg-orange-400":
+                nums==="C"?"bg-red-500 hover:bg-red-400":
+                nums==="+"||nums==="-"||nums==="*"||nums==="/"?"bg-green-500 hover:bg-green-400":
+                ""} `} >{nums}</button>)}
+        </div>
+        
+}
+
 export function Calculator(){
-    return <h1>cal</h1>
+
+    const [answer,setAnswer]=useState("")
+    const buttonValues= ["1","2","3","+","4","5","6","-","7","8","9",,"C","*","0","/","="]
+    return <>
+        <div  className='flex items-center justify-center flex-col gap-4 mt-12'>
+            <div className=' bg-purple-400 rounded-md p-4'>
+                <Header answer={answer} setAnswer={setAnswer} />
+                <Buttons buttonValues={buttonValues} answer={answer} setAnswer={setAnswer} />
+                </div>
+            </div>
+    </>
 }
--- studentos/src/Task.jsx
+++ studentos/src/Task.jsx
@@ -1,5 +1,5 @@
 import { useState,useEffect,useRef } from "react";
-
+import checkMark from "./images/check-mark.png"
 function Header({setTask,text,setText,editId,setEditId,inputRef}){
     
 
@@ -7,6 +7,9 @@ function Header({setTask,text,setText,editId,setEditId,inputRef}){
        setText(event.target.value);
     }
     function addTask(){
+        if(text.trim()===""){
+            return;
+        }
         setTask((prev)=> {
             const newState=[...prev,{id:crypto.randomUUID(),text:text,compeleted:false}];
             return newState;
@@ -14,6 +17,9 @@ function Header({setTask,text,setText,editId,setEditId,inputRef}){
         setText("");
     }
     function editTask(){
+        if(text.trim()===""){
+            return;
+        }
         setTask((prev)=>{
             const newState = prev.map((item)=>
                 editId===item.id?{...item,text:text}:item 
@@ -25,14 +31,19 @@ function Header({setTask,text,setText,editId,setEditId,inputRef}){
 
     }
     return (
-        <>
-        <input value={text} onChange={inputToTask} onKeyDown={(event)=>{
+        <div className="flex justify-center items-center gap-[50px] p-[25px]">
+        <input placeholder="Task" value={text} onChange={inputToTask} onKeyDown={(event)=>{
             if(event.key==="Enter"){
                 editId?editTask():addTask()
             }
-        }} ref={inputRef} />
-        <button onClick={editId==null? addTask : editTask}>{editId===null ? "+add" : "save" }</button>
-        </>
+        }} ref={inputRef} className="border rounded-[16px] w-[45%] h-[50px] pl-[12px] text-[20px]" />
+        <button onClick={editId==null? addTask : editTask} 
+        className="border-2 h-[50px] w-[120px] rounded-[16px] flex justify-center items-center 
+        border-blue-50 bg-blue-500 text-white font-extrabold transition-all duration-150
+        hover:bg-blue-400 hover:border-blue-50  hover:font-black 
+        active:bg-blue-600 active:border-blue-200 active:translate-y-[1px] "
+        >{editId===null ? "+ADD" : "SAVE" }</button>
+        </div>
     )
 }
 function ShowTask({tasks,setCompeleted,text,editId,setEditId,setText,inputRef}){
@@ -43,12 +54,16 @@ function ShowTask({tasks,setCompeleted,text,editId,setEditId,setText,inputRef}){
          tasks.map((task,index)=>{
 
             function delTask(id){
+
                 setCompeleted(prev=>{
                    const newState= prev.filter(item=> 
                         item.id!==id
                     )
                     return newState;
-             } )
+                })
+                setText("")
+                setEditId(null)
+
             } 
             function editTask(id,text){
                 setEditId(id);
@@ -56,7 +71,7 @@ function ShowTask({tasks,setCompeleted,text,editId,setEditId,setText,inputRef}){
                 inputRef.current.focus()
             }
             return (
-                <div key={task.id}>
+                <div key={task.id} className="flex gap-8 justify-center items-center p-1 mt-1 ">
                 <button onClick={()=>{
                     setCompeleted( (prev) =>{
                         const newState= prev.map((item)=>{
@@ -64,10 +79,22 @@ function ShowTask({tasks,setCompeleted,text,editId,setEditId,setText,inputRef}){
                      } )
                      return newState;
                     })
-                }}> {task.compeleted?"compeleted":"not compeleted"}</button> 
-                <span>{task.text}</span>
-                <button onClick={()=> editTask(task.id,task.text)}>EDIT</button>
-                <button onClick={()=> delTask(task.id)} >DEL</button>
+                }} className={`w-8 h-8 border rounded-full ${task.compeleted?"bg-green-400":"bg-red-600"}`} >
+                    {task.compeleted?<img src={checkMark} className="w-8 h-8 -mt-4 ml-2" />:""}
+                     </button> 
+
+                <span className="h-12 flex w-1/2 pt-1 text-right overflow-hidden text-3xl font-sans capitalize">{task.text}</span>
+
+                <button onClick={()=> editTask(task.id,task.text)} className="bg-blue-500 text-white 
+                    border w-24 p-4 rounded-[10px] 
+                    hover:bg-blue-400
+                    active:bg-blue-600  active:translate-y-[1px]" 
+                    >EDIT</button>
+                <button onClick={()=> delTask(task.id)}  className="bg-red-500 text-white 
+                    border w-24 p-4 rounded-[10px]
+                    hover:bg-red-400
+                    active:bg-red-600  active:translate-y-[1px] " 
+                    >DEL</button>
                 </div>
             )
          })
@@ -86,8 +113,10 @@ function Todo(){
     const inputRef=useRef(null)
     return (
         <>
-        <Header setTask={setTasks} text={text} setText={setText} editId={editId} setEditId={setEditId} inputRef={inputRef} />
-        <ShowTask tasks={tasks} setCompeleted={setTasks} text={text} editId={editId} setEditId={setEditId} setText={setText} inputRef={inputRef} />
+        <Header setTask={setTasks} text={text} setText={setText} editId={editId} setEditId={setEditId}
+         inputRef={inputRef} />
+        <ShowTask tasks={tasks} setCompeleted={setTasks} text={text} editId={editId} setEditId={setEditId} 
+        setText={setText} inputRef={inputRef} />
         </>
     )
 }
